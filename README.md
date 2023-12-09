f72d03b4dc4baa1e6e7de4b9bdd8e55fd8fcc33e
Saffi <saffi@saffi.dev>
2023-12-09 13:06:04 +0000

    Implement day 9 part 2

--- README.md
+++ README.md
@@ -26,17 +26,17 @@ Spöttel](https://github.com/fspoettel).
 
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
-| [Day 1](./src/bin/01.rs) | `53.4µs` | `547.3µs` |
-| [Day 2](./src/bin/02.rs) | `39.8µs` | `37.6µs` |
-| [Day 3](./src/bin/03.rs) | `441.3µs` | `422.6µs` |
-| [Day 4](./src/bin/04.rs) | `205.9µs` | `185.5µs` |
-| [Day 5](./src/bin/05.rs) | `19.1µs` | `48.2µs` |
-| [Day 6](./src/bin/06.rs) | `427.0ns` | `12.5ms` |
-| [Day 7](./src/bin/07.rs) | `3.0ms` | `2.8ms` |
-| [Day 8](./src/bin/08.rs) | `378.7µs` | `2.5ms` |
-| [Day 9](./src/bin/09.rs) | `138.6µs` | `-` |
+| [Day 1](./src/bin/01.rs) | `46.1µs` | `552.8µs` |
+| [Day 2](./src/bin/02.rs) | `38.5µs` | `37.8µs` |
+| [Day 3](./src/bin/03.rs) | `427.7µs` | `421.6µs` |
+| [Day 4](./src/bin/04.rs) | `193.0µs` | `185.0µs` |
+| [Day 5](./src/bin/05.rs) | `18.0µs` | `48.1µs` |
+| [Day 6](./src/bin/06.rs) | `288.0ns` | `12.4ms` |
+| [Day 7](./src/bin/07.rs) | `2.9ms` | `2.8ms` |
+| [Day 8](./src/bin/08.rs) | `365.8µs` | `2.5ms` |
+| [Day 9](./src/bin/09.rs) | `136.4µs` | `128.5µs` |
 
-**Total: 23.32ms**
+**Total: 23.20ms**
 <!--- benchmarking table --->
 
 ---
--- src/bin/09.rs
+++ src/bin/09.rs
@@ -4,7 +4,7 @@ use std::str::FromStr;
 
 advent_of_code::solution!(9);
 
-fn predict(nums: &[i64]) -> i64 {
+fn predict_next(nums: &[i64]) -> i64 {
     if nums.iter().all(|&n| n == 0) {
         0
     } else {
@@ -12,7 +12,7 @@ fn predict(nums: &[i64]) -> i64 {
             .array_windows::<2>()
             .map(|[l, r]| r - l)
             .collect::<Vec<_>>();
-        let next_diff = predict(&diffs);
+        let next_diff = predict_next(&diffs);
 
         nums.last().unwrap() + next_diff
     }
@@ -27,13 +27,37 @@ pub fn part_one(input: &str) -> Option<i64> {
                     .flat_map(i64::from_str)
                     .collect::<Vec<_>>()
             })
-            .map(|nums| predict(&nums))
+            .map(|nums| predict_next(&nums))
             .sum(),
     )
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+fn predict_prev(nums: &[i64]) -> i64 {
+    if nums.iter().all(|&n| n == 0) {
+        0
+    } else {
+        let diffs = nums
+            .array_windows::<2>()
+            .map(|[l, r]| r - l)
+            .collect::<Vec<_>>();
+        let prev_diff = predict_prev(&diffs);
+
+        nums.first().unwrap() - prev_diff
+    }
+}
+
+pub fn part_two(input: &str) -> Option<i64> {
+    Some(
+        input
+            .lines()
+            .map(|line| {
+                line.split_whitespace()
+                    .flat_map(i64::from_str)
+                    .collect::<Vec<_>>()
+            })
+            .map(|nums| predict_prev(&nums))
+            .sum(),
+    )
 }
 
 #[cfg(test)]
@@ -49,6 +73,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2));
     }
 }
